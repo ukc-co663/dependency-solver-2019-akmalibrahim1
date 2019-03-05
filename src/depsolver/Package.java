b74500f32c24cc06f7150c143832808214a24ec2
maai2 <maai2@kent.ac.uk>
2019-03-05 18:51:25 +0000

    Revert "Revert "Add constants file to handle operator strings and include/exclude definitions""
    This reverts commit 782c5e7e8aeac00ea92b176d789bbd069b077dec.

--- src/depsolver/Package.java
+++ src/depsolver/Package.java
@@ -1,9 +1,10 @@
 package depsolver;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class Package {
+public class Package implements Comparable<Package> {
     private String name;
     private String version;
     private Integer size;
@@ -48,5 +49,55 @@ public class Package {
 
     public void setConflicts(List<String> conflicts) {
         this.conflicts = conflicts;
+    }
+
+    /**
+     * Checks to see if the package version matches the expected dependency version
+     * @param expectedVersion The version number that is expected
+     * @param givenVersion The version that is given
+     * @param comparator THe comparison for the package version
+     * @return
+     */
+    public static boolean checkVersion(String expectedVersion, String givenVersion, String comparator){
+        String [] ev = expectedVersion.split("\\.");
+        String [] gv = givenVersion.split("\\.");
+        boolean validVersion = true;
+        int maxLoop =  ev.length < gv.length ? gv.length : ev.length;
+        for(int i = 0; i < maxLoop; i++){
+
+            int e = i < ev.length ? Integer.parseInt(ev[i]) : 0;
+            int v = i < gv.length ? Integer.parseInt(gv[i]) : 0;
+            switch(comparator){
+                case Constants.EQUAL:
+                    if(v != e)
+                        validVersion = false;
+                    break;
+                case Constants.GREATER_OR_EQUAL:
+                    if(v < e)
+                        validVersion = false;
+                    break;
+                case Constants.LESS_OR_EQUAL:
+                    if(v > e)
+                        validVersion = false;
+                    break;
+                case Constants.GREATER_THAN:
+                    if(v <= e)
+                        validVersion = false;
+                    break;
+                case Constants.LESS_THAN:
+                    if(v >= e)
+                        validVersion = false;
+                    break;
+            }
+            if(validVersion == false)
+                break;
+        }
+
+        return validVersion;
+    }
+
+    @Override
+    public int compareTo(Package o) {
+        return this.name.compareTo(o.getName());
     }
 }
