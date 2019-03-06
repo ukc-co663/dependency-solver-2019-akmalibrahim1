2a654f2fc1bca34c6f2c4ec790f91da1b6ed2ccf
maai2 <maai2@kent.ac.uk>
2019-03-06 16:53:42 +0000

    Remove unused methods and declarations

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -19,9 +19,8 @@ public class Main {
     };
     List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
     Resolver r = new Resolver(repo, JSON.parseObject(readFile(args[2]), strListType));
+    r.Run();
     List<String> con = JSON.parseObject(readFile(args[2]), strListType);
-    HashMap<SatPackage, Boolean> constraints = getConstraints(con);
-    constraints.forEach((e, f) -> System.out.println(e.toString() + " State = " + f));
   }
 
   static String readFile(String filename) throws IOException {
@@ -30,40 +29,4 @@ public class Main {
     br.lines().forEach(line -> sb.append(line));
     return sb.toString();
   }
-
-  static HashMap<SatPackage, Boolean> getConstraints(List<String> constraints){
-      HashMap<SatPackage, Boolean> result = new HashMap<>();
-      for(String c : constraints){
-        String version = null;
-        String cTemp = c;
-        String compare = null;
-        if(cTemp.contains("=")){
-            String [] split = cTemp.split("=");
-            cTemp = split[0];
-            version = split[1];
-            compare = Constants.EQUAL;
-        }
-            //flag that states whether the package should be installed or removed
-            boolean add = false;
-            if (cTemp.charAt(0) == '+') {
-                add = true;
-            }
-            result.put(new SatPackage(version, cTemp.substring(1)), add);
-      }
-      return result;
-  }
 }
-
-
-// CHANGE CODE BELOW:
-// using repo, initial and constraints, compute a solution and print the answer
-//    for (Package p : repo) {
-//            System.out.printf("package %s version %s\n", p.getName(), p.getVersion());
-//            for (List<String> clause : p.getDepends()) {
-//        System.out.printf("  dep:");
-//        for (String q : clause) {
-//        System.out.printf(" %s", q);
-//        }
-//        System.out.printf("\n");
-//        }
-//        }
