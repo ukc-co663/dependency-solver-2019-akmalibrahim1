13e836854eff71e2fc06da44e7b1224edc768628
maai2 <maai2@kent.ac.uk>
2019-03-07 13:31:57 +0000

    add SatPackage to hold final sat packages

--- src/depsolver/SatPackage.java
+++ src/depsolver/SatPackage.java
@@ -1,24 +1,4 @@
 package depsolver;
 
 public class SatPackage {
-    private String version;
-    private String packageName;
-
-    public SatPackage(String version, String packageName){
-        this.version = version;
-        this.packageName = packageName;
-    }
-
-    public String getVersion(){
-        return version;
-    }
-
-    public String getPackageName(){
-        return packageName;
-    }
-
-    @Override
-    public String toString (){
-        return "Package = " + packageName + " Version = " + version;
-    }
 }
