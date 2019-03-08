5ae8611bc01ac54d09ff903a2522d9b81a73b2ae
maai2 <maai2@kent.ac.uk>
2019-03-08 17:29:02 +0000

    Remove download line from the installer

--- install_deps.sh
+++ install_deps.sh
@@ -6,7 +6,6 @@ apt-get update -qq -y
 apt-get install binutils g++ make ant -y
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-download, compile and install Z3
 Z3_DIR="$(mktemp -d)"
 cd "$Z3_DIR"
 wget -qO- https://github.com/Z3Prover/z3/archive/z3-${Z3_VERSION}.tar.gz | tar xz --strip-components=1
