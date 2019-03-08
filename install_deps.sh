0790b738e14ca3d9f884b430335e31326740b04e
maai2 <maai2@kent.ac.uk>
2019-03-08 19:11:25 +0000

    Remove pwd and add ls after examples have been built

--- install_deps.sh
+++ install_deps.sh
@@ -4,10 +4,10 @@ mkdir -p lib
 wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
 git clone https://github.com/Z3Prover/z3.git
 cd z3
-pwd
 python scripts/mk_make.py --java
 cd build
 make examples
+pwd
 cp ./com.microsoft.z3.jar ../../lib
 make
 sudo make install
