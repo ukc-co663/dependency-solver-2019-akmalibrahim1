daebf11f217c8bc8b685151bfbd0e3c875911cb5
maai2 <maai2@kent.ac.uk>
2019-03-08 17:26:57 +0000

    Updateinstaller to use wget and download z3

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 rm -rf lib/*
 mkdir -p lib
-curl -o lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
+wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
+apt-get update -qq -y
+apt-get install binutils g++ make ant -y
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+download, compile and install Z3
+Z3_DIR="$(mktemp -d)"
+cd "$Z3_DIR"
+wget -qO- https://github.com/Z3Prover/z3/archive/z3-${Z3_VERSION}.tar.gz | tar xz --strip-components=1
+python scripts/mk_make.py --java
+cd build
+make
+make install
+cd /
+rm -rf "$Z3_DIR"
