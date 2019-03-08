f306325877f28623a1ca64789fc6c7262bf0b09f
maai2 <maai2@kent.ac.uk>
2019-03-08 18:48:06 +0000

    Add command to print current working directory

--- install_deps.sh
+++ install_deps.sh
@@ -4,8 +4,8 @@ mkdir -p lib
 wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
 git clone https://github.com/Z3Prover/z3.git
 cd z3
+pwd
 python scripts/mk_make.py --java
-ls build
 cp build/com.microsoft.z3.jar ../lib
 cd build
 make
