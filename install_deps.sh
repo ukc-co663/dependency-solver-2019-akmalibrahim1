b4911a9d892aaa1f12ecd3e6e3873b3a34e07950
maai2 <maai2@kent.ac.uk>
2019-03-08 17:51:15 +0000

    Update install commands

--- install_deps.sh
+++ install_deps.sh
@@ -2,16 +2,10 @@
 rm -rf lib/*
 mkdir -p lib
 wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
-apt-get update -qq -y
-apt-get install binutils g++ make ant -y
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-Z3_DIR="$(mktemp -d)"
-cd "$Z3_DIR"
-wget -qO- https://github.com/Z3Prover/z3/archive/z3-${Z3_VERSION}.tar.gz | tar xz --strip-components=1
+git clone https://github.com/Z3Prover/z3.git
+cd z3
 python scripts/mk_make.py --java
+cp build/com.microsoft.z3.jar ../lib
 cd build
 make
-make install
-cd /
-rm -rf "$Z3_DIR"
+sudo make install
